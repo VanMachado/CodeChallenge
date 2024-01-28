c9bb9fc609e21f0de53a8879a5628ee6246256ac
VanMachado <vandamme.machado@gmail.com>
2024-01-27 22:21:46 -0300

    Finalizacao do desafio 'Menor Distancia'

--- MenorDistanciaArray/Program.cs
+++ MenorDistanciaArray/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenorDistancia
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> array1 = new List<int>();
+            List<int> array2 = new List<int>();
+            int resultado;
+
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    Random random = new Random();
+                    int numeroAleatorio = random.Next(100);
+
+                    if (j == 0)
+                        array1.Add(numeroAleatorio);
+                    if (j == 1)
+                        array2.Add(numeroAleatorio);
+                }
+            }
+
+            if (array1[array1.Count - 1] - array2[array2.Count - 1] < 0)
+                resultado = array2[array2.Count - 1] - array1[array1.Count - 1];
+            else
+                resultado = array1[array1.Count - 1] - array2[array2.Count - 1];
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+ReadMe
+
+https://www.mycompiler.io/pt/new/csharp - para o primeiro desafio.
+
+https://www.mycompiler.io/pt/new/csharp - para o segundo, colocar no campo "Entrada do programa" o seguinte valor.
+12
+1,0,15.4,50
+2,0,15.5,50
+2,2,0,0
+2,0,15.4,10
+3,0,15.9,30
+3,1,0,20
+4,0,16.50,200
+5,0,17.00,100
+5,0,16.59,20
+6,2,0,0
+1,2,0,0
+2,1,15.6,0
+
+https://www.mycompiler.io/pt/new/csharp - para o terceiro, basta copiar o codigo no copilador e executar, assim como o primeiro.
